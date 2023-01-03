7aaa7ef0654b6d52d86bb894fa4811eedfcda254
prakash19csu214 <prakashsingh112000@gmail.com>
2023-01-03 18:13:32 +0530

    add to cart button

--- src/Components/ListCard/index.js
+++ src/Components/ListCard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './index.css'
+import "./index.css";
 
 function ListCard({ product }) {
   return (
@@ -14,21 +14,24 @@ function ListCard({ product }) {
         </div>
       </div>
       <div className="col-11 col-md-5 mt-5 ml-2">
-          <div className="row text-center featured-head my-2">{product.title}</div>
-          <div className="row text-center featured-para">
-            <b> $ {product.price} </b>&nbsp; &nbsp;
-          </div>
-          <div className="list-para row my-1">
-            {product.description}
-          </div>
-            <div className="row text-center featured-para my-2">
+        <div className="row text-center featured-head my-2">
+          {product.title}
+        </div>
+        <div className="row text-center featured-para">
+          <b> $ {product.price} </b>&nbsp; &nbsp;
+        </div>
+        <div className="list-para row my-1">{product.description}</div>
+        {/* <div className="row text-center featured-para my-2">
             <span className="fa list-icon fa-shopping-cart"></span>
             <span className="fa list-icon fa-heart-o"></span>
             <span className="fa list-icon fa-search-plus"></span>
-          </div>
-          
-          
-          </div>
+          </div> */}
+        <div className="btnn2 mt-2">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            <span className="btnn2-text">Add To Card</span>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
